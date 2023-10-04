aa44688caf91c06d7f1957e73f13b432526d7506
Arjunan <63443618+Arjunan1234@users.noreply.github.com>
2023-10-04 18:05:34 +0530

    Update Rocketlaunch.java
    updated the code by adding the initial state message

--- Rocketlaunch.java
+++ Rocketlaunch.java
@@ -1,1 +1,14 @@
+public class Rocketlaunch {
+    public static void main(String[] args) {
+        String stage = "Pre-Launch";
+        int fuelPercentage = 100;
+        int altitude = 0;
+        int speed = 0;
+
+        System.out.println("Stage: \"" + stage + "\"");
+        System.out.println("Fuel: " + fuelPercentage + "%");
+        System.out.println("Altitude: " + altitude + " km");
+        System.out.println("Speed: " + speed + " km/h");
+    }
+}
 
